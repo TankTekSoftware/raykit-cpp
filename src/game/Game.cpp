4d182ec019fc3893848eec6e1f66cf203e02b07d
msmith-codes <msmith.softwaredev@gmail.com>
2025-12-01 12:24:00 -0500

    Removed Networking

--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -13,7 +13,6 @@ Game::Game()
     this->window_config.height = 600;
     this->window_config.title = "raykit-cpp - Starter Template";
     this->window_config.target_fps = 60;
-
 }
 
 void Game::on_compose()
--- src/game/net/PacketType.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-enum class PacketType : uint16_t 
-{
-    NONE = 0,
-    // Add more packet types as needed
-};
